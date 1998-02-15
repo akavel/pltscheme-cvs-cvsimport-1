4fe7553d5e450f8429795b5330608dfec58e5192
mflatt <mflatt>
1998-02-15 22:48:11 +0000

    help menu and arcs

--- src/wxmac/src/base/wb_item.cc
+++ src/wxmac/src/base/wb_item.cc
@@ -476,33 +476,7 @@ wxbMenuBar::~wxbMenuBar (void)
 
 void wxbMenuBar::Append (wxMenu * menu, char *title)
 {
-  if (!OnAppend(menu, title))  // mflatt
-         return;
-
-  n++;
-  wxMenu **new_menus = new wxMenu *[n];
-  char **new_titles = new char *[n];
-
-  int i;
-  for (i = 0; i < n - 1; i++)
-    {
-      new_menus[i] = menus[i];
-      menus[i] = NULL;
-      new_titles[i] = titles[i];
-      titles[i] = NULL;
-    }
-  if (menus)
-    {
-      delete[]menus;
-      delete[]titles;
-    }
-  menus = new_menus;
-  titles = new_titles;
-
-  menus[n - 1] = menu;
-  titles[n - 1] = copystring (title);
-
-  menu->menu_bar = (wxMenuBar *) this;
+  OnAppend(menu, title);
 }
 
 void wxbMenuBar::Delete(wxMenu * menu, int i)
@@ -524,12 +498,6 @@ void wxbMenuBar::Delete(wxMenu * menu, int i)
 
   if (!OnDelete(menu, i))
          return;
-
-  --n;
-  for (j = i; j < n; j++) {
-         menus[j] = menus[j + 1];
-         titles[j] = titles[j + 1];
-  }
 }
 
 // Find the menu menuString, item itemString, and return the item id.
