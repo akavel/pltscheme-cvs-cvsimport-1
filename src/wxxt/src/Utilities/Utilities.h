52c7d500f8787cb1aaee9c6da6372862b466bc79
mflatt <mflatt>
2000-05-02 15:14:45 +0000

    .

--- src/wxxt/src/Utilities/Utilities.h
+++ src/wxxt/src/Utilities/Utilities.h
@@ -66,7 +66,7 @@ Bool wxGetUserName(char *buf, int sz);
 // String functions
 char *copystring(const char *s);
 #ifdef MZ_PRECISE_GC
-char *copystring_to_aligned(const char *s);
+char *copystring_to_aligned(const char *s, int d);
 #endif
 void wxGetLabelAndKey(char *label, char **clean_label, char **clean_key);
 char *wxStripMenuCodes(char *in, char *out);
