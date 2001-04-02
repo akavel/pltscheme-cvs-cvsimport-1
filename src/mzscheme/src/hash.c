1fcfc162986b476438e0866b7edf7525ee3442ec
mflatt <mflatt>
2001-04-02 15:22:18 +0000

    .

--- src/mzscheme/src/hash.c
+++ src/mzscheme/src/hash.c
@@ -42,8 +42,10 @@
 #define FILL_FACTOR 2
 #endif
 
+#define MIN_HTABLE_SIZE 7
+
 long scheme_hash_primes[] = 
-{7, 31, 127, 257, 521, 1031, 2053, 4099, 8209, 16411, 
+{MIN_HTABLE_SIZE, 31, 127, 257, 521, 1031, 2053, 4099, 8209, 16411, 
    32779, 65543, 131101, 262147, 425329, 1048583, 2097169,
    4194319, 8388617, 16777259, 33554467, 67108879, 134217757,
    268435459, 536870923, 1073741827};
@@ -51,6 +53,10 @@ long scheme_hash_primes[] =
 typedef int (*Hash_Compare_Proc)(void*, void*);
 
 typedef long hash_v_t;
+
+/*========================================================================*/
+/*                         hashing functions                              */
+/*========================================================================*/
 
 static void string_hash_indices(void *_key, long *_h, long *_h2)
 {
@@ -96,6 +102,10 @@ static int not_stx_bound_eq(char *a, char *b)
   return !scheme_stx_bound_eq((Scheme_Object *)a, (Scheme_Object *)b, 0);
 }
 
+/*========================================================================*/
+/*                         normal hash table                              */
+/*========================================================================*/
+
 static Scheme_Object GONE[1];
 
 Scheme_Hash_Table *scheme_make_hash_table(int type)
@@ -133,18 +143,19 @@ static Scheme_Object *do_hash(Scheme_Hash_Table *table, Scheme_Object *key, int 
 {
   Scheme_Object *tkey, **keys;
   hash_v_t h, h2, useme = 0;
+  long size = table->size;
 
  rehash_key:
 
   if (table->make_hash_indices) {
     table->make_hash_indices((void *)key, &h, &h2);
-    h = h % table->size;
-    h2 = h2 % table->size;
+    h = h % size;
+    h2 = h2 % size;
   } else {
     long lkey;
     lkey = PTR_TO_LONG((Scheme_Object *)key);
-    h = (lkey >> 2) % table->size;
-    h2 = (lkey >> 3) % table->size;
+    h = (lkey >> 2) % size;
+    h2 = (lkey >> 3) % size;
   }
 
   if (h < 0) h = -h;
@@ -173,7 +184,7 @@ static Scheme_Object *do_hash(Scheme_Hash_Table *table, Scheme_Object *key, int 
 	} else
 	  return table->vals[h];
       }
-      h = (h + h2) % table->size;
+      h = (h + h2) % size;
     }
   } else {
     while ((tkey = keys[h])) {
@@ -190,7 +201,7 @@ static Scheme_Object *do_hash(Scheme_Hash_Table *table, Scheme_Object *key, int 
 	  useme = h;
 	}
       } 
-      h = (h + h2) % table->size;
+      h = (h + h2) % size;
     }
   }
 
@@ -200,25 +211,26 @@ static Scheme_Object *do_hash(Scheme_Hash_Table *table, Scheme_Object *key, int 
   if (set == 1) {
     h = useme;
     --table->count; /* counter increment below */
-  } else if (table->count * FILL_FACTOR >= table->size) {
+  } else if (table->count * FILL_FACTOR >= size) {
     /* Rehash */
     int i, oldsize = table->size;
     Scheme_Object **oldkeys = table->keys;
     Scheme_Object **oldvals = table->vals;
 
     table->size = scheme_hash_primes[++table->step];
+    size = table->size;
     
     {
       Scheme_Object **ba;
-      ba = MALLOC_N(Scheme_Object *, table->size);
+      ba = MALLOC_N(Scheme_Object *, size);
       table->vals = ba;
-      ba = MALLOC_N(Scheme_Object *, table->size);
+      ba = MALLOC_N(Scheme_Object *, size);
       table->keys = ba;
     }
 
     table->count = 0;
     for (i = 0; i < oldsize; i++) {
-      if (oldkeys[i])
+      if (oldkeys[i] && !SAME_PTR(oldkeys[i], GONE))
 	do_hash(table, oldkeys[i], 2, oldvals[i]);
     }
 
@@ -277,7 +289,9 @@ Scheme_Object *scheme_hash_get(Scheme_Hash_Table *table, Scheme_Object *key)
   return val;
 }
 
-/************************************************************************/
+/*========================================================================*/
+/*                  old-style hash table, with buckets                    */
+/*========================================================================*/
 
 Scheme_Bucket_Table *
 scheme_make_bucket_table (int size, int type)
@@ -551,7 +565,9 @@ scheme_change_in_table (Scheme_Bucket_Table *table, const char *key, void *naya)
     bucket->val = naya;
 }
 
-/***********************************************************************/
+/*========================================================================*/
+/*                         precise GC hashing                             */
+/*========================================================================*/
 
 #ifdef MZ_PRECISE_GC
 
