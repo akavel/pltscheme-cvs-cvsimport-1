b3d015dcadf811a6e75485437ca4ddc7e451084d
mflatt <mflatt>
2001-07-25 20:23:24 +0000

    .

--- src/wxxt/src/EventHandling/wb_stdev.cc
+++ src/wxxt/src/EventHandling/wb_stdev.cc
@@ -249,7 +249,7 @@ Bool wxMouseEvent::Leaving(void)
   return (eventType == wxEVENT_TYPE_LEAVE_WINDOW) ;
 }
 
-void wxMouseEvent::Position(float *xpos, float *ypos)
+void wxMouseEvent::Position(int *xpos, int *ypos)
 {
   *xpos = x;
   *ypos = y;
@@ -295,7 +295,7 @@ long wxKeyEvent::KeyCode(void)
   return keyCode;
 }
 
-void wxKeyEvent::Position(float *xpos, float *ypos)
+void wxKeyEvent::Position(int *xpos, int *ypos)
 {
   *xpos = x;
   *ypos = y;
