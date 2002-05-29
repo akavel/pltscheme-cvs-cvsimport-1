1afe42c68ff1d7e8757d94d2932190f019be45d8
mflatt <mflatt>
2002-05-29 12:21:13 +0000

    .

--- src/wxmac/src/base/wb_dc.cc
+++ src/wxmac/src/base/wb_dc.cc
@@ -4,13 +4,8 @@
  * Author:      Julian Smart
  * Created:     1993
  * Updated:	August 1994
- * RCS_ID:      $Id$
  * Copyright:   (c) 1993, AIAI, University of Edinburgh
  */
-
-/* static const char sccsid[] = "@(#)wb_dc.cc      1.2 5/9/94"; */
-
-// #include "wx.h" // Uncomment this line for Borland precomp. headers to work
 
 #ifdef __GNUG__
 #ifdef __GNUG__
