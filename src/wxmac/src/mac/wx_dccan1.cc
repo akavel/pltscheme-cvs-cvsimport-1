dc282ee6f852cb439d584ef80d07a66d1d1ad2e3
mflatt <mflatt>
2005-03-13 15:24:52 +0000

    .

--- src/wxmac/src/mac/wx_dccan1.cc
+++ src/wxmac/src/mac/wx_dccan1.cc
@@ -726,10 +726,9 @@ void wxCanvasDC::wxMacSetCurrentTool(wxMacToolType whichTool)
       pw = current_pen->GetWidthF();
       if (AlignSmoothing()) {
 	pw = (int)pw;
+	pw = (int)(pw * user_scale_x);
 	if (!pw)
 	  pw = 1;
-	else
-	  pw = (int)(pw * user_scale_x);
       } else {
 	if (!pw) {
 	  if (user_scale_x > user_scale_y)
@@ -980,10 +979,9 @@ double wxCanvasDC::GetPenSmoothingOffset()
 {
   int pw;
   pw = current_pen->GetWidth();
+  pw = (int)(user_scale_x * pw);
   if (!pw)
     pw = 1;
-  else
-    pw = (int)(user_scale_x * pw);
   return ((pw & 1) * 0.5);
 }
 
