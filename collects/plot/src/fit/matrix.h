a058b1b9c3e98593402604b65c3db2770d2c0eaf
eli <eli>
2005-03-15 23:19:05 +0000

    .

--- collects/plot/src/fit/matrix.h
+++ collects/plot/src/fit/matrix.h
@@ -48,20 +48,20 @@
 
 /******* public functions ******/
 
-//MZ_DLLEXPORT
+MZ_DLLEXPORT
 EXT double  *vec (int n);
-//MZ_DLLEXPORT
+MZ_DLLEXPORT
 EXT int     *ivec (int n);
-//MZ_DLLEXPORT
+MZ_DLLEXPORT
 EXT double  **matr (int r, int c);
-//MZ_DLLEXPORT
+MZ_DLLEXPORT
 EXT double  *redim_vec (double **v, int n);
-//MZ_DLLEXPORT
+MZ_DLLEXPORT
 EXT void    redim_ivec (int **v, int n);
 EXT void    solve (double **a, int n, double **b, int m);
-//MZ_DLLEXPORT
+MZ_DLLEXPORT
 EXT void    Givens (double **C, double *d, double *x, double *r, int N, int n, int want_r); 
-//MZ_DLLEXPORT
+MZ_DLLEXPORT
 EXT void    Invert_RtR (double **R, double **I, int n);
 
 #endif
